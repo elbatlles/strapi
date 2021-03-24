5e25282868099ef370357c99e4188bb360e1fc74
angel <angel.batlles@kumux.io>
2021-03-24 16:12:23 +0100

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9e1dd6c1295b093100c3f834ff7370ef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1a6f9a0f-919d-4f30-a651-2ebc728fb81c'
+};
